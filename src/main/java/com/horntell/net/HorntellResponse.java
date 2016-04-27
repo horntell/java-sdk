38d675bb9f450fbd15f753c05cfddb9d2fc6a443
Devendra <devendra-tiwari@hotmail.com>
2016-04-27 11:16:27 +0530

    Removed gson dependency

--- src/main/java/com/horntell/net/HorntellResponse.java
+++ src/main/java/com/horntell/net/HorntellResponse.java
@@ -5,46 +5,40 @@
  */
 package com.horntell.net;
 
-import com.google.gson.Gson;
 import com.squareup.okhttp.ResponseBody;
+
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 
 public class HorntellResponse {
-	
-    ResponseBody responseOrignal;
-	int responseCode;
-	String responseBody;
+
     public HashMap<String, Object> responseMap;
-	
-	public HorntellResponse(com.squareup.okhttp.Response response) throws IOException {
-            this.responseOrignal = response.body();
-            System.out.println("response code");            
-            System.out.println(response.code());
-            this.responseCode = response.code();
-            this.responseBody = response.body().string();
-	}
+    ResponseBody responseOrignal;
+    int responseCode;
+    String responseBody;
+
+    public HorntellResponse(com.squareup.okhttp.Response response) throws IOException {
+        this.responseOrignal = response.body();
+        System.out.println("response code");
+        System.out.println(response.code());
+        this.responseCode = response.code();
+        this.responseBody = response.body().string();
+    }
 
     HorntellResponse(HorntellResponse response) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     public int getStatusCode() {
-            return responseCode;
+        return responseCode;
     }
 
     public String getBody() {
-            return responseBody;
+        return responseBody;
     }
 
-    public ResponseBody getOriginal()
-    {
+    public ResponseBody getOriginal() {
         return responseOrignal;
-    }
-
-    public Map<String,Object> getResponseMap(){
-        return new Gson().fromJson(responseBody,Map.class);
     }
 
 

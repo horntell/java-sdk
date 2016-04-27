d3a921da1999a0049eb9c28d87df654a32b0c021
Devendra <devendra-tiwari@hotmail.com>
2016-04-27 13:34:11 +0530

    refactored code for exception

--- src/main/java/com/horntell/exception/ForbiddenException.java
+++ src/main/java/com/horntell/exception/ForbiddenException.java
@@ -1,6 +1,6 @@
 package com.horntell.exception;
 
-public class ForbiddenException extends Exception {
+public class ForbiddenException extends InvalidRequestException {
     public ForbiddenException(String message, Integer code, String type) {
         super(message, code, type);
     }
--- src/main/java/com/horntell/exception/NotFoundException.java
+++ src/main/java/com/horntell/exception/NotFoundException.java
@@ -1,10 +1,6 @@
 package com.horntell.exception;
 
-public class NotFoundException extends Exception {
-
-	public NotFoundException(String message, Integer code, String type, Throwable e) {
-		super(message, code, type, e);
-	}
+public class NotFoundException extends InvalidRequestException {
 
     public NotFoundException(String message, Integer code, String type) {
         super(message, code, type);
